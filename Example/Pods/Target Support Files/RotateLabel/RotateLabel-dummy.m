75667d16175ec7b7055c850e41e5eecc3bda7d83
Anju Singh Yadav <ayadav49@sapient.com>
2017-02-03 10:11:23 +0530

    Intial Commit

---
+++ Example/Pods/Target Support Files/RotateLabel/RotateLabel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RotateLabel : NSObject
+@end
+@implementation PodsDummy_RotateLabel
+@end
--- Example/RotateLabel/ViewController.swift
+++ Example/RotateLabel/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var rotatingLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        rotatingLabel.frame = CGRect(x: UIScreen.mainScreen().bounds.size.width/2 - 50, y: UIScreen.mainScreen().bounds.size.height / 2 - 50 , width: 100, height: 100 )
+        self.view.addSubview(rotatingLabel)
+        rotatingLabel.backgroundColor = UIColor.orangeColor()
+        
+        self.rotateView(rotatingLabel)
     }
-
+    
+    private func rotateView(targetView: UILabel, duration: Double = 1.0) {
+        
+        UIView.animateWithDuration(duration, delay: 0.0, options: UIViewAnimationOptions.CurveLinear, animations: { () -> Void in
+            
+            targetView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI));
+            
+            }, completion: { (finished: Bool) -> Void in
+                self.rotateView(targetView, duration: duration)
+        })
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
